47ac8f4594ae24ad3f4739f69fe78fe98c116a25
ito_tomohiro <tomohiro.ito@jp.fujitsu.com>
2014-10-17 16:23:35 +0900

    NAM-71 YOUTUBE動画一覧取得
    ・youtube動画一覧の取得を実施
    ・福島テレビチャンネル動画一覧の表示処理を実施
    
    担当：滝田、伊藤

--- www_dev/app/modules/view/top/TopView.js
+++ www_dev/app/modules/view/top/TopView.js
@@ -5,18 +5,104 @@ define(function(require, exports, module) {
 
     var TopView = Backbone.Layout.extend({
         template : require("ldsh!/app/templates/top/top"),
+        /**
+         * youtubePlayer
+         **/
+        player : null,
+        events : {
+            "click .playListItem":"onClickPlayList"
+        },
 
         beforeRender : function() {
 
         },
 
         afterRender : function() {
+            var tag = document.createElement('script');
+            
+            tag.src = "https://www.youtube.com/iframe_api";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+            setTimeout($.proxy(function () {
+                this.setYouTubePlayer();
+            },this),1200);
 
         },
-
+        /**
+         * 初期化処理
+         */
         initialize : function() {
-
+            var tag = document.createElement('script');
+            
+            tag.src = "https://apis.google.com/js/client.js";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        },
+        /**
+         * youtube動画プレイヤーの設定
+         */
+        setYouTubePlayer : function () {
+            this.player = new YT.Player('youtubePlayer', {
+                width: '640',
+                height: '390',
+                events:{
+                    "onReady": $.proxy(this.onSetYouTubePlayer,this)
+                }
+            });
+        },
+        onSetYouTubePlayer : function () {
+            gapi.client.setApiKey("AIzaSyCfqTHIGvjra1cyftOuCP9-UGZcT9YkfqU");
+            gapi.client.load('youtube', 'v3', $.proxy(this.searchPlayList,this));
+        },
+        searchPlayList : function () {
+            this.channelsVideos("UC9_ZCtgOk8dPC6boqZMNqbw");
+        },
+        channelsVideos : function(channelId) {
+            gapi.client.request({
+                path : "/youtube/v3/search",
+                mine : false,
+                params : {
+                    channelId : channelId,
+                    type : "video",
+                    part : "id,snippet",
+                    maxResults : 5,
+                    order : "date",
+                },
+                callback : $.proxy(this.setPlayList,this)
+            });
+        },
+        /**
+         * 取得した動画一覧を描画する
+         */
+        setPlayList : function (res) {
+            var playList = res.items;
+            this.setVideo(playList[0]);
+            _.each(playList, $.proxy(function (item) {
+                var elem = $('<li class="playListItem" style="white-space: pre;border-top:solid 1px;"><span class="videoTitle" style="font-size: small;font-weight:bold;"></span><img/></li><span class="descriptionText" style="font-size: small;"></span>');
+                elem.find("img").attr("src",item.snippet.thumbnails["default"].url);
+                elem.attr("videoId",item.id.videoId);
+                elem.find(".videoTitle").text(item.snippet.title);
+                $(elem[1]).text(item.snippet.description);
+                $("#playListUl").append(elem);
+            },this));
+            
+        },
+        onClickPlayList : function(ev) {
+            var item = {
+                    id : {videoId :$(ev.currentTarget).attr("videoId")},
+                    snippet : {
+                        title: $(ev.currentTarget).text(),
+                        description: $(ev.currentTarget).next().text()
+                    }
+            };
+            this.setVideo(item);
+        },
+        setVideo :function (video) {
+            this.player.loadVideoById(video.id.videoId);
+            $("#videoTitle").text(video.snippet.title);
+            $("#videoDescription").text(video.snippet.description);
         }
+
     });
 
     module.exports = TopView;
