4227fdedc06188daefedbf2d7beaab2d50601510
ito_tomohiro <tomohiro.ito@jp.fujitsu.com>
2014-10-24 16:36:38 +0900

    NAM-59 記事詳細表示
    ・createdAtのparseを実施
    
    担当：滝田、伊藤

--- www_dev/app/modules/collection/article/ArticleCollection.js
+++ www_dev/app/modules/collection/article/ArticleCollection.js
@@ -4,6 +4,7 @@ define(function(require, exports, module) {
     var app = require("app");
     var AbstractODataCollection = require("modules/collection/AbstractODataCollection");
     var ArticleModel = require("modules/model/article/ArticleModel");
+    var DateUtil = require("modules/util/DateUtil");
 
     /**
      * 記事情報のコレクションクラス
@@ -14,6 +15,12 @@ define(function(require, exports, module) {
         condition : {
             top : 10,
             orderby : "createdAt desc"
+        },
+        parseOData: function (response, options) {
+            _.each(response,function (res) {
+                res.createdAt = DateUtil.formatDate(new Date(res.createdAt),"yyyy年MM月dd日 HH時mm分")
+            })
+            return response;
         },
     });
 
