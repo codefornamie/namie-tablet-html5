c004f72621389c438713aea2f3d6f967e4db8c67
ito_tomohiro <tomohiro.ito@jp.fujitsu.com>
2014-10-30 14:41:13 +0900

    NAM-100 記事保存:お気に入りデータ保存
    ・お気に入り登録ボタンが押下された際の保存処理を作成
    ・お気に入り情報Model,collection作成
    ・お気に入り情報が既に登録されている記事のボタン非表示処理を作成
    
    担当：増田、伊藤

---
+++ www_dev/app/modules/util/Utils.js
@@ -0,0 +1,14 @@
+define(function(require, exports, module) {
+    "use strict";
+
+    var Utils = function() {
+
+    };
+    Utils.getLoginUser = function() {
+        return {
+                    name:"namie"
+                };
+    };
+
+    module.exports = Utils;
+});
