c4091d73d4946b03825b41e158a0f999fc37b1f1
Keiji Yahata <yahata@nowhere.co.jp>
2014-11-06 14:32:30 +0900

    NAM-147 記事に対するアニメーションディレイの処理を削除

--- www_dev/app/modules/view/news/ArticleListView.js
+++ www_dev/app/modules/view/news/ArticleListView.js
@@ -55,30 +55,6 @@ define(function(require, exports, module) {
                 model : model,
                 template: template
             }));
-
-            /*
-            var animationDeley = 0;
-            this.collection.each($.proxy(function(model) {
-                var template = require("ldsh!/app/templates/news/articleListItem");
-                switch (model.get("modelType")) {
-                    case "youtube":
-                        template = require("ldsh!/app/templates/news/articleListItem");
-                        break;
-                    case "event":
-                        template = require("ldsh!/app/templates/news/eventsListItem");
-                        break;
-                    default:
-                        template = require("ldsh!/app/templates/news/articleListItem");
-                        break;
-                }
-                this.insertView("#articleList", new ArticleListItemView({
-                    model : model,
-                    template: template,
-                    animationDeley : animationDeley
-                }));
-                animationDeley += 0.2;
-            }, this));
-            */
         }
     });
 
