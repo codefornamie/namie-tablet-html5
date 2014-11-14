c1119099a717963fe18b35205b643eec6dd52125
Manabu Sakaguchi <sakaguchi.manab@jp.fujitsu.com>
2014-11-14 17:20:55 +0900

    NAM-238 イベント：基盤作成(ログインまで)
    カスタマイズしたlodash-template-loader をrequirejsに指定
    担当：阪口、藤田

--- www_dev/app/config.js
+++ www_dev/app/config.js
@@ -3,7 +3,7 @@ require.config({
         "dc1-client" : "../lib/dao/dc1-client",
         "underscore" : "../bower_components/lodash/dist/lodash.underscore",
         "lodash" : "../bower_components/lodash/dist/lodash",
-        "ldsh" : "../bower_components/lodash-template-loader/loader",
+        "ldsh" : "../lib/lodash-template-loader/loader",
         "jquery" : "../bower_components/jquery/dist/jquery",
         "backbone" : "../bower_components/backbone/backbone",
         "layoutmanager" : "../bower_components/layoutmanager/backbone.layoutmanager",
