23b915926e148192ccd41cd8cc0232433ad30f6a
Manabu Sakaguchi <sakaguchi.manab@jp.fujitsu.com>
2014-11-17 11:06:19 +0900

    NAM-238 イベント：基盤作成(ログインまで)
    postingモードでログインするとテンプレートファイルエラーとなる問題を修正
    担当：阪口

--- www_dev/app/modules/view/events/ShowEventsView.js
+++ www_dev/app/modules/view/events/ShowEventsView.js
@@ -8,7 +8,7 @@ define(function(require, exports, module) {
     var EventsDetailView = require("modules/view/events/EventsDetailView");
 
     var EventsView = AbstractView.extend({
-        template : require("ldsh!/app/templates/events/showEvents"),
+        template : require("ldsh!templates/{mode}/events/showEvents"),
 
         beforeRendered : function() {
 
