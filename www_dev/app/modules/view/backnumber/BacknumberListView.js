ab1540fed4cb4d865a05106f131bdc7688c354cb
Keiji Yahata <yahata@nowhere.co.jp>
2014-11-19 21:37:32 +0900

    NAM-216 バックナンバーのリストが出るようにする

--- www_dev/app/modules/view/backnumber/BacknumberListView.js
+++ www_dev/app/modules/view/backnumber/BacknumberListView.js
@@ -3,6 +3,7 @@ define(function(require, exports, module) {
 
     var app = require("app");
     var AbstractView = require("modules/view/AbstractView");
+    var BacknumberListItemView = require("modules/view/backnumber/BacknumberListItemView");
 
     /**
      *  バックナンバーのリストのView
@@ -21,6 +22,7 @@ define(function(require, exports, module) {
          *  ViewのテンプレートHTMLの描画処理が完了する前に呼び出される。
          */
         beforeRendered : function() {
+            this.setBacknumberList();
         },
 
         /**
@@ -33,12 +35,25 @@ define(function(require, exports, module) {
          *  初期化処理
          */
         initialize : function() {
-
         },
 
         events : {
-        }
+        },
 
+        /**
+         *  バックナンバー一覧を描画する
+         */
+        setBacknumberList: function () {
+            var self = this;
+
+            this.collection.each(function (model) {
+                var backnumberListView = new BacknumberListView();
+
+                self.insertView('#backnumber-list', new BacknumberListItemView({
+                    model: model
+                }));
+            });
+        }
     });
     module.exports = BacknumberListView;
 });
--- www_dev/app/modules/view/backnumber/BacknumberView.js
+++ www_dev/app/modules/view/backnumber/BacknumberView.js
@@ -3,6 +3,8 @@ define(function(require, exports, module) {
 
     var app = require("app");
     var AbstractView = require("modules/view/AbstractView");
+    var BacknumberListView = require("modules/view/backnumber/BacknumberListView");
+    var BacknumberCollection = require("modules/collection/backnumber/BacknumberCollection");
 
     /**
      *  バックナンバーページのView
@@ -33,7 +35,10 @@ define(function(require, exports, module) {
          *  初期化処理
          */
         initialize : function() {
+            var backnumberListView = new BacknumberListView();
 
+            backnumberListView.collection = new BacknumberCollection();
+            this.setView('#backnumber', backnumberListView);
         },
 
         events : {
