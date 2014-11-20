b20b7aed7b95c5b5b378ae6e7824ef28c2c6db54
fj8730jh <fj8730jh@aa.jp.fujitsu.com>
2014-11-20 20:39:37 +0900

    NAM-242 イベント登録編集処理実装
    以下の処理を実装した。
    - イベント登録処理
    - 確認画面
    
    担当：伊藤、黒澤

--- www_dev/app/modules/view/posting/news/ArticleRegistView.js
+++ www_dev/app/modules/view/posting/news/ArticleRegistView.js
@@ -4,8 +4,11 @@ define(function(require, exports, module) {
     var app = require("app");
     var AbstractView = require("modules/view/AbstractView");
     var ArticleRegistFileItemView = require("modules/view/posting/news/ArticleRegistFileItemView");
+    var ArticleRegistConfirmView = require("modules/view/posting/news/ArticleRegistConfirmView");
     var FileAPIUtil = require("modules/util/FileAPIUtil");
     var DateUtil = require("modules/util/DateUtil");
+    var ArticleModel = require("modules/model/article/ArticleModel");
+    var vexDialog = require("vexDialog");
 
     /**
      * 記事新規登録画面のViewクラス
@@ -16,6 +19,10 @@ define(function(require, exports, module) {
      */
     var ArticleRegistView = AbstractView.extend({
         template : require("ldsh!templates/{mode}/news/articleRegist"),
+        /**
+         * このビューのModel
+         */
+        model: null,
         /**
          * フォーム要素のID
          */
@@ -68,60 +75,122 @@ define(function(require, exports, module) {
          */
         onClickArticleRegistButton : function() {
             if ($(this.formId).validate().form()) {
-                this.showLoading();
-                // this.onSubmit();
+                var errmsg = this.validate();
+                if (errmsg) {
+                    vexDialog.defaultOptions.className = 'vex-theme-default';
+                    vexDialog.alert(errmsg);
+                } else {
+                    this.onSubmit();
+                }
             }
+        },
+        /**
+         * バリデーションチェック
+         */
+        validate : function(){
+            if($("#articleMultiDate").is(":checked")){
+                if($("#articleDate1").val() > $("#articleDate2").val()){
+                    return "日時の日付が開始と終了で逆になっています。";
+                }
+            }
+            if ($("#articleTime1").val() && $("#articleTime2").val()
+                    && $("#articleTime1").val() > $("#articleTime2").val()) {
+                return "日時の時間が開始と終了で逆になっています。";
+            }
+            if ($("#articleRangeDate1").val() && $("#articleRangeDate2").val()
+                    && $("#articleRangeDate1").val() > $("#articleRangeDate2").val()) {
+                return "掲載期間の日付が開始と終了で逆になっています。";
+            }
+            return null;
         },
         /**
          * モデルにデータをセットする関数
          */
-        setInputValue : function() {
-        },
-        /**
-         * 添付された画像をdavへ登録する
-         */
-        saveArticlePicture : function() {
-            var reader = new FileReader();
-            var contentType = "";
-            reader.onload = $.proxy(function(fileEvent) {
-                var options = {
-                    body : fileEvent.currentTarget.result,
-                    headers : {
-                        "Content-Type" : contentType,
-                        "If-Match" : "*"
+        setInputValue : function(callback) {
+            if(this.model === null){
+                this.model = new ArticleModel(); 
+            }
+            this.model.set("type", $("#articleCategory").val());
+            this.model.set("title", $("#articleTitle").val());
+            
+            this.model.set("startDate", $("#articleDate1").val());
+            if ($("#articleMultiDate").is(":checked")) {
+                this.model.set("endDate", $("#articleDate2").val());
+            }
+            this.model.set("startTime", $("#articleTime1").val());
+            this.model.set("endTime", $("#articleTime2").val());
+
+            this.model.set("place", $("#articlePlace").val());
+            this.model.set("description", $("#articleDetail").val());
+            this.model.set("contactInfo", $("#articleContact").val());
+            
+            this.model.set("publishedAt", $("#articleRangeDate1").val());
+            this.model.set("depublishedAt", $("#articleRangeDate2").val());
+            
+            this.model.set("status", "0");
+            this.model.set("createUserId", app.user.get("__id"));
+            
+            var imageCount = this.$el.find("#fileArea").children().size();
+            if(imageCount === 0){
+                callback();
+                return;
+            }
+            this.showLoading();
+            var images = [];
+            var self = this;
+            _.each(this.$el.find("#fileArea").children(), function(fileItem){
+                var file = $(fileItem).find("#articleFile").prop("files")[0];
+                if(!file){
+                    imageCount--;
+                    if(imageCount <= 0){
+                        self.hideLoading();
+                        callback();
+                    }
+                    return;
+                }
+                var image = {};
+                var preName = file.name.substr(0, file.name.lastIndexOf("."));
+                var suffName = file.name.substr(file.name.lastIndexOf("."));
+                image.fileName = preName + "_" + String(new Date().getTime()) + suffName;
+                image.contentType = file.type;
+
+                var reader = new FileReader();
+                reader.onload = function(fileEvent){
+                    image.data = fileEvent.currentTarget.result;
+                    imageCount--;
+                    if(imageCount <= 0){
+                        self.hideLoading();
+                        callback();
                     }
                 };
-                app.box.col("dav").put(this.fileName, options);
-            }, this);
-            var file = $("#articleFile").prop("files")[0];
-            contentType = file.type;
-            var preName = file.name.substr(0, file.name.lastIndexOf("."));
-            var suffName = file.name.substr(file.name.lastIndexOf("."));
-            this.fileName = preName + "_" + String(new Date().getTime()) + suffName;
-            reader.readAsArrayBuffer(file);
+                reader.onerror = function(){
+                    self.hideLoading();
+                    vexDialog.defaultOptions.className = 'vex-theme-default';
+                    vexDialog.alert("ファイルの読み込みに失敗しました。");
+                }
+                reader.readAsArrayBuffer(file);
+                
+                image.comment = $(fileItem).find("#articleFileComent").val();
+                image.blob = $(fileItem).find("#previewFile").attr("src");
+                images.push(image);
+            });
+            this.model.set("images", images);
+            for(var i = 0; i < images.length; i++){
+                var surfix = (i === 0) ? "" : "" + (i + 1);
+                this.model.set("imageUrl" + surfix, images[i].fileName);
+                this.model.set("imageComment" + surfix, images[i].comment);
+            }
         },
         /**
          * バリデーションチェックがOKとなり、登録処理が開始された際に呼び出されるコールバック関数。
          */
         onSubmit : function() {
-            try {
-                // 登録処理を開始する
-                if ($('#previewFile').attr("src")) {
-                    this.saveArticlePicture();
-                }
-                this.setInputValue();
-                var self = this;
-                this.model.save(null, {
-                    success : function() {
-                        setTimeout(function() {
-                            self.hideLoading();
-                            app.router.go("eventsRegisted");
-                        }, 1000);
-                    }
-                });
-            } catch (e) {
-                return;
-            }
+            // 登録処理を開始する
+            this.setInputValue($.proxy(function(){
+                $("#articleRegistPage").hide();
+                this.setView("#articleRegistConfirmWrapperPage", new ArticleRegistConfirmView({model: this.model})).render();
+                $("#snap-content").scrollTop(0)
+            }, this));
         },
 
     });
