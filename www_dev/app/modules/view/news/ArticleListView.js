9b4cf1bc9d6fd44b37f480eb491ed29225c87479
Keiji Yahata <yahata@nowhere.co.jp>
2014-11-25 11:58:20 +0900

    NAM-216 iScrollは不要になったため削除する

--- www_dev/app/modules/view/news/ArticleListView.js
+++ www_dev/app/modules/view/news/ArticleListView.js
@@ -1,9 +1,5 @@
-/* global IScroll:true */
-
 define(function(require, exports, module) {
     "use strict";
-
-    require('iscroll-zoom');
 
     var app = require("app");
     var AbstractView = require("modules/view/AbstractView");
@@ -23,7 +19,6 @@ define(function(require, exports, module) {
         _currentArticleId: null,
 
         beforeRendered : function() {
-            this.destroyIScroll();
             this.setArticleList();
         },
 
@@ -51,116 +46,6 @@ define(function(require, exports, module) {
             app.off('scrollToArticle:articleList');
             app.off('willChangeFontSize:articleList');
             app.off('didChangeFontSize:articleList');
-        },
-
-        /**
-         * iScrollを初期化する
-         */
-        initIScroll: function () {
-            var self = this;
-            var $container = $('#contents__primary');
-            var $img = $container.find('img');
-            var count = $img.length;
-
-            $img.each(function () {
-                var img = this;
-
-                // すでに読み込まれている場合はカウントを減らす
-                if (this.src && this.complete) {
-                    $(this).attr({
-                        width: this.width,
-                        height: this.height
-                    });
-
-                    count--;
-                    registerIScroll();
-                    return;
-                }
-
-                // 画像が読み込まれたらカウントを減らす
-                $(this)
-                    .on('load.iscroll', function () {
-                        $(this).attr({
-                            width: this.width,
-                            height: this.height
-                        });
-
-                        count--;
-                        registerIScroll();
-                    })
-                    .on('error.iscroll', function () {
-                        count--;
-                        registerIScroll();
-                    });
-
-                // しばらく読み込まれなかったら無視する
-                setTimeout(function () {
-                    $(img).off('load.iscroll').off('error.iscroll');
-
-                    count--;
-                    registerIScroll();
-                }, 5000);
-            });
-
-            if ($img.length === 0) {
-                registerIScroll();
-            }
-
-            function registerIScroll() {
-                if (count > 0) return;
-
-                _initIScroll();
-
-                // 読み込み後のフェードインのため
-                $('.article-list').addClass('is-ready');
-            }
-
-            function _initIScroll() {
-                // ページ遷移後はスクロール位置を0にする
-                $container.animate({
-                    scrollTop: 0
-                }, 0);
-
-                // iscrollインスタンスを生成する
-                self.iscroll = new IScroll($container[0], {
-                    scrollbars: true,
-                    zoom: true,
-                    probeType: 1
-                });
-
-                // scroll量に従ってページ切り替えを行う
-                var SCROLL_BUFFER = 50;
-
-                self.iscroll.on('scroll', function () {
-                    var y = self.iscroll.y;
-                    var scrollTop = $container.scrollTop();
-                    var containerHeight = $container.height();
-                    var contentHeight = $container.children().height();
-                    var hiddenHeight = contentHeight - (scrollTop + containerHeight);
-
-                    if (0 < y) {
-                        if (SCROLL_BUFFER < y) {
-                            self.goToPreviousPage();
-                        }
-                    } else {
-                        y = y + hiddenHeight;
-
-                        if (y < -SCROLL_BUFFER) {
-                            self.goToNextPage();
-                        }
-                    }
-                });
-            }
-        },
-
-        /**
-         * 生成済みのiscrollインスタンスを破棄する
-         */
-        destroyIScroll: function () {
-            if (this.iscroll) {
-                this.iscroll.destroy();
-                this.iscroll = null;
-            }
         },
 
         /**
