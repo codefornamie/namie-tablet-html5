97b483b05b174a055c1ab404729104b14f02d978
ito_tomohiro <tomohiro.ito@jp.fujitsu.com>
2014-11-25 16:16:28 +0900

    NAM-261
    ・新規記事登録処理を作成
    ・youtube用確認画面を作成
    
    
    担当：藤田、伊藤

--- www_dev/app/modules/view/posting/news/ArticleRegistConfirmView.js
+++ www_dev/app/modules/view/posting/news/ArticleRegistConfirmView.js
@@ -39,7 +39,7 @@ define(function(require, exports, module) {
             // 掲載期間
             var pubDateString = DateUtil.formatDate(new Date(this.model.get("publishedAt")),"yyyy年MM月dd日(ddd)");
             if(this.model.get("depublishedAt")){
-                pubDateString += " ～ " + DateUtil.formatDate(new Date(this.model.get("endDate")) ,"yyyy年MM月dd日(ddd)");
+                pubDateString += " ～ " + DateUtil.formatDate(new Date(this.model.get("depublishedAt")) ,"yyyy年MM月dd日(ddd)");
             }
             $("#articlePublishRange").text("掲載期間： " + pubDateString);
             
@@ -48,8 +48,12 @@ define(function(require, exports, module) {
             
             var imgs = $("#articleImageArea img");
             var imgIndex = 0;
-            _.each(this.model.get("images"), function(image){
-                $(imgs[imgIndex++]).attr("src", image.blob);
+            var previewUrlArr = [];
+            _.each($("img#previewFile"),function(elem) {
+                previewUrlArr.push($(elem).attr("src"));
+            });
+            _.each(previewUrlArr, function(url){
+                $(imgs[imgIndex++]).attr("src", url);
             });
         },
 
@@ -109,7 +113,6 @@ define(function(require, exports, module) {
         saveModel : function(){
             this.model.save(null, {
                 success : $.proxy(function() {
-                    this.hideLoading();
                     app.router.go("posting-top");
                 }, this),
                 error: function(e){
