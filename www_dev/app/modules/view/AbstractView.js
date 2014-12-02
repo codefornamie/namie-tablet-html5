d8a73a4e4df20f8baaf92d39cb6dbc9a04db63d2
Manabu Sakaguchi <sakaguchi.manab@jp.fujitsu.com>
2014-12-02 17:16:24 +0900

    NAM-321 新聞：左上におすすめ記事表示
    おすすめ記事がイベントの場合でも、画像を正しく表示するように修正。
    担当：阪口

--- www_dev/app/modules/view/AbstractView.js
+++ www_dev/app/modules/view/AbstractView.js
@@ -2,6 +2,8 @@ define(function(require, exports, module) {
     "use strict";
 
     var app = require("app");
+    var FileAPIUtil = require("modules/util/FileAPIUtil");
+
     /**
      * 全てのViewの基底クラスを作成する。
      * 
@@ -32,9 +34,14 @@ define(function(require, exports, module) {
          * サブクラスは本メソッドをオーバライドして、
          * 描画前の独自の処理を実装できる。
          * </p>
+         * @memberof AbstractView#
          */
         beforeRendered : function() {
         },
+        /**
+         * 要素の描画処理が完了した際に呼び出される。
+         * @memberof AbstractView#
+         */
         afterRender : function() {
             this.afterRendered();
             if (this.formId) {
@@ -58,17 +65,16 @@ define(function(require, exports, module) {
          * サブクラスは本メソッドをオーバライドして、
          * 描画後の独自の処理を実装できる。
          * </p>
+         * @memberof AbstractView#
          */
         afterRendered : function() {
             
         },
-        initialize : function() {
 
-        },
-
-        events : {
-
-        },
+        /**
+         * ローディングメッセージを表示する。
+         * @memberof AbstractView#
+         */
         showLoading : function () {
             $.blockUI({
                 message: "しばらくお待ちください",
@@ -83,8 +89,49 @@ define(function(require, exports, module) {
                 }
             }); 
         },
+        /**
+         * ローディングメッセージを閉じる
+         * @memberof AbstractView#
+         */
         hideLoading :function () {
             $.unblockUI();
+        },
+        /**
+         * 指定されたimg要素に、imgArrayパラメタで指定された画像のコンテンツを表示する。
+         * 
+         * @param {String} imgElementSelector 画像を表示する要素のセレクタ。<br/>
+         * このセレクタで取得される要素の数と、imgArrayの要素数は一致していなければならない。
+         * @param {Array} imgArray 画像情報を含むオブジェクトの配列。<br/>
+         * 以下のプロパティを含める。<br/>
+         * imageUrl: 画像のDAVのファイルパス<br/>
+         * imageIndex: 画像のインデックス<br/>
+         * @memberof AbstractView#
+         */
+        showPIOImages : function(imgElementSelector, imgArray) {
+            var onGetBinary = $.proxy(function(binary,item) {
+                var articleImage = $(this.el).find(imgElementSelector);
+                var arrayBufferView = new Uint8Array(binary);
+                var blob = new Blob([ arrayBufferView ], {
+                    type : "image/jpg"
+                });
+                var url = FileAPIUtil.createObjectURL(blob);
+                $(articleImage[item.imageIndex-1]).load(function() {
+                    window.URL.revokeObjectURL($(this).attr("src"));
+                });
+                $(articleImage[item.imageIndex-1]).attr("src", url);
+                $(articleImage[item.imageIndex-1]).data("blob", blob);
+            },this);
+            _.each(imgArray,$.proxy(function (item) {
+                try {
+                    app.box.col("dav").getBinary(item.imageUrl, {
+                        success : $.proxy(function(binary) {
+                            onGetBinary(binary,item);
+                        },this)
+                    });
+                } catch (e) {
+                    console.error(e);
+                }
+            },this));
         }
     });
 
--- www_dev/app/modules/view/news/EventListItemView.js
+++ www_dev/app/modules/view/news/EventListItemView.js
@@ -3,13 +3,13 @@ define(function(require, exports, module) {
 
     var app = require("app");
     var ArticleListItemView = require("modules/view/news/ArticleListItemView");
-    var FileAPIUtil = require("modules/util/FileAPIUtil");
+    // var FileAPIUtil = require("modules/util/FileAPIUtil");
     var DateUtil = require("modules/util/DateUtil");
     var CommonUtil = require("modules/util/CommonUtil");
 
     /**
      * 記事一覧アイテムのViewを作成する。
-     *
+     * 
      * @class 記事一覧アイテムのView
      * @exports EventListItemView
      * @constructor
@@ -17,6 +17,7 @@ define(function(require, exports, module) {
     var EventListItemView = ArticleListItemView.extend({
         /**
          * このViewを表示する際に利用するアニメーション
+         * @memberof EventListItemView#
          */
         template : require("ldsh!templates/{mode}/news/eventsDetail"),
         /**
@@ -24,19 +25,20 @@ define(function(require, exports, module) {
          * <p>
          * 記事に関連する画像ファイルの取得と表示を行う。
          * </p>
+         * @memberof EventListItemView#
          */
         afterRendered : function() {
             this.showImage();
             this.afterRenderCommon();
-            var dateString = DateUtil.formatDate(new Date(this.model.get("startDate")),"yyyy年MM月dd日(ddd)");
-            if(this.model.get("endDate")){
-                dateString += " ～ " + DateUtil.formatDate(new Date(this.model.get("endDate")),"yyyy年MM月dd日(ddd)");
+            var dateString = DateUtil.formatDate(new Date(this.model.get("startDate")), "yyyy年MM月dd日(ddd)");
+            if (this.model.get("endDate")) {
+                dateString += " ～ " + DateUtil.formatDate(new Date(this.model.get("endDate")), "yyyy年MM月dd日(ddd)");
             }
             var st = this.model.get("startTime");
             st = st ? st : "";
             var et = this.model.get("endTime");
             et = et ? et : "";
-            if(st || et){
+            if (st || et) {
                 dateString += "\n" + st + " ～ " + et;
             }
             $(this.el).find("#articleDateTime").html(CommonUtil.sanitizing(dateString));
@@ -44,50 +46,26 @@ define(function(require, exports, module) {
         },
         /**
          * このViewが表示している記事に関連する画像データの取得と表示を行う。
+         * @memberof EventListItemView#
          */
         showImage : function() {
-            var onGetBinary = $.proxy(function(binary,item) {
-                var articleImage = $(this.el).find(".eventFileImage img");
-                var arrayBufferView = new Uint8Array(binary);
-                var blob = new Blob([ arrayBufferView ], {
-                    type : "image/jpg"
-                });
-                var url = FileAPIUtil.createObjectURL(blob);
-                $(articleImage[item.imageIndex-1]).load(function() {
-//                    $(this).show();
-                    window.URL.revokeObjectURL($(this).attr("src"));
-                });
-                $(articleImage[item.imageIndex-1]).attr("src", url);
-                $(articleImage[item.imageIndex-1]).data("blob", blob);
-            },this);
-
             var imgArray = [];
-            for (var i=1;i<4;i++) {
+            for (var i = 1; i < 4; i++) {
                 var index = i;
                 if (i === 1) {
                     index = "";
                 }
                 if (this.model.get("imageUrl" + index)) {
                     imgArray.push({
-                        imageUrl:this.model.get("imageUrl" + index),
-                        imageComment:this.model.get("imageComment" + index),
-                        imageIndex:i
+                        imageUrl : this.model.get("imageUrl" + index),
+                        imageComment : this.model.get("imageComment" + index),
+                        imageIndex : i
                     });
                 } else {
-                    $($(this.el).find(".eventFileImage img")[i-1]).parent().parent().hide();
+                    $($(this.el).find(".eventFileImage img")[i - 1]).parent().parent().hide();
                 }
             }
-            _.each(imgArray,$.proxy(function (item) {
-                try {
-                    app.box.col("dav").getBinary(item.imageUrl, {
-                        success : $.proxy(function(binary) {
-                            onGetBinary(binary,item);
-                        },this)
-                    });
-                } catch (e) {
-                    console.error(e);
-                }
-            },this));
+            this.showPIOImages(".eventFileImage img", imgArray);
         },
     });
     module.exports = EventListItemView;
