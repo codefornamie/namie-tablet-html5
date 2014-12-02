55696e5459dc736b799840f3fc80af360808833e
fj8730jh <fj8730jh@aa.jp.fujitsu.com>
2014-12-02 20:22:28 +0900

    NAM-346 depublishedAtを参照して記事の表示期間を正しく反映する
    - depublishedAtを参照して記事の表示期間を正しく反映するようにした
    - 上記に応じてイイねの情報取得を最適化した
    - 新聞の発行日を発行時刻が経過するまで進めないようにした
    - NewsViewのパラメタで外部から新聞の発行日を指定可能とした
    
    担当：黒澤、伊藤

--- www_dev/app/modules/view/news/NewsView.js
+++ www_dev/app/modules/view/news/NewsView.js
@@ -8,6 +8,7 @@ define(function(require, exports, module) {
     var FeedListView = require("modules/view/news/FeedListView");
     var RecommendArticleView = require("modules/view/news/RecommendArticleView");
     var DateUtil = require("modules/util/DateUtil");
+    var BusinessUtil = require("modules/util/BusinessUtil");
     var ArticleModel = require("modules/model/article/ArticleModel");
     var ArticleCollection = require("modules/collection/article/ArticleCollection");
     var RecommendCollection = require("modules/collection/article/RecommendCollection");
@@ -15,6 +16,10 @@ define(function(require, exports, module) {
     var EventsCollection = require("modules/collection/events/EventsCollection");
     var Equal = require("modules/util/filter/Equal");
     var IsNull = require("modules/util/filter/IsNull");
+    var And = require("modules/util/filter/And");
+    var Or = require("modules/util/filter/Or");
+    var Le = require("modules/util/filter/Le");
+    var Ge = require("modules/util/filter/Ge");
 
     /**
      * 記事一覧・詳細のメインとなる画面のViewクラスを作成する。
@@ -40,8 +45,10 @@ define(function(require, exports, module) {
         },
 
         initialize : function() {
+            this.targetDate = this.targetDate ||
+                    DateUtil.formatDate(BusinessUtil.getCurrentPublishDate(), "yyyy-MM-dd");
             this.setArticleSearchCondition({
-                targetDate : new Date()
+                targetDate : new Date(this.targetDate)
             });
             this.searchArticles();
         },
@@ -71,10 +78,20 @@ define(function(require, exports, module) {
             var targetDate = condition.targetDate;
             var dateString = DateUtil.formatDate(targetDate, "yyyy-MM-dd");
             this.articleCollection.condition.filters = [
-                                                        new Equal("publishedAt", dateString),
-                                                        new IsNull("isDepublish")
-                                                                ];
-            this.recommendCollection.condition.filters = new Equal("publishedAt", dateString);
+                    new Or([
+                            new Equal("publishedAt", dateString), new And([
+                                    new Le("publishedAt", dateString), new Ge("depublishedAt", dateString)
+                            ])
+                    ]), new IsNull("isDepublish")
+            ];
+
+            this.recommendCollection.condition.filters = [
+                new Or([
+                        new Equal("publishedAt", dateString), new And([
+                                new Le("publishedAt", dateString), new Ge("depublishedAt", dateString)
+                        ])
+                ])
+            ];
             this.eventsCollection.condition.filters = new Equal("publishedAt", dateString);
         },
         /**
@@ -110,8 +127,7 @@ define(function(require, exports, module) {
                 });
             };
             async.series([
-               onLoadGAPI,
-               loadScript("https://www.youtube.com/iframe_api")
+                    onLoadGAPI, loadScript("https://www.youtube.com/iframe_api")
             ], function(err) {
                 if (!err) {
                     app.gapiLoaded = true;
@@ -207,6 +223,9 @@ define(function(require, exports, module) {
 
             // articleCollectionに切抜き、おすすめ状態を反映する
             this.newsCollection.each($.proxy(function(article) {
+                if (article.get("title", "おすすめイベント")) {
+                    console.log("aaa");
+                }
                 this.favoriteCollection.each(function(favorite) {
                     if (article.get("__id") === favorite.get("source")) {
                         article.set("isFavorite", !favorite.get("deletedAt"));
@@ -225,7 +244,7 @@ define(function(require, exports, module) {
                 // 自身のおすすめ情報を記事に付加
                 _.each(recommends, $.proxy(function(recommend) {
                     if (recommend.get("isMine")) {
-                        article.set("isRecommend", !recommend.get("deletedAt"));
+                        article.set("isMyRecommend", !recommend.get("deletedAt"));
                         article.recommend = recommend;
                     }
                 }, this));
@@ -234,7 +253,7 @@ define(function(require, exports, module) {
 
             // おすすめ記事を検索し、表示する
             var recommendArticle = this.newsCollection.find($.proxy(function(article) {
-                return article.get("isRecommend") === "true";
+                return article.get("isRecommend") === "true" && article.get("publishedAt") === this.targetDate;
             }, this));
 
             if (recommendArticle) {
