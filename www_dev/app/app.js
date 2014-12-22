2d198ef40adf8c8abff01de5502478e0cc5de9b6
Keiji Yahata <yahata@nowhere.co.jp>
2014-12-22 17:49:59 +0900

    サブディレクトリ下でも動作するようにリンク等のURLを指定する
    作業者：八幡

--- www_dev/app/app.js
+++ www_dev/app/app.js
@@ -19,7 +19,7 @@ define(function(require, exports, module) {
     var app = module.exports = new Backbone.Model();
 
     // The root path to run the application through.
-    app.root = "/";
+    app.root = "/www_dev/";
     // アプリの設定情報を保持
     app.config = config;
 
