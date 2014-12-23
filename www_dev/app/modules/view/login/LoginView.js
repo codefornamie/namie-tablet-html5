7304e7152f061ea2f79243d335a01e361f4788ae
Manabu Sakaguchi <sakaguchi.manab@jp.fujitsu.com>
2014-12-24 03:26:23 +0900

    NAM-363 リリース：CI環境デプロイジョブ設定
    認証情報取得処理の遅延時間を変更
    担当：阪口

--- www_dev/app/modules/view/login/LoginView.js
+++ www_dev/app/modules/view/login/LoginView.js
@@ -21,7 +21,7 @@ define(function(require, exports, module) {
             "click #loginButton" : "onClickLoginButton"
         },
         beforeRendered : function() {
-            setTimeout($.proxy(this.onTimeout, this), 500);
+            setTimeout($.proxy(this.onTimeout, this), 1000);
         },
 
         afterRendered : function() {
