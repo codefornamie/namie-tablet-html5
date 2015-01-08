4e36d249aecc7534a2c9b786fac9b5a9640383cb
Kensuke Shimizu <shimizu@nowhere.co.jp>
2015-01-08 13:57:40 +0900

    NAM-664 おたより：おたより情報を１つの記事化(HTML、CSS)
    ・おたより＆イベント記事項目（EventListItemView）で、画像が正常に表示されるよう修正
    
    担当：真水

--- www_dev/app/modules/view/news/EventListItemView.js
+++ www_dev/app/modules/view/news/EventListItemView.js
@@ -49,34 +49,44 @@ define(function(require, exports, module) {
          * @memberOf EventListItemView#
          */
         showImage : function() {
-            var modelArray = [];
             var imgArray = [];
 
             if (this.model.get("letters")) {
-                // おたより記事のリストを持っている場合は、その内容を対象とする
-                modelArray = this.model.get("letters");
-            } else {
-                // その他の場合はモデル自身がもつ画像情報を対象とする
-                modelArray.push(this.model);
-            }
+                // おたより記事のリストを持っている場合（おたより一覧記事の場合）は、その内容を対象とする
+                var modelArray = this.model.get("letters");
+                var index = 1;
 
-            _.each(modelArray, $.proxy(function(model) {
-                for (var i = 1; i < 4; i++) {
-                    var index = i;
-                    if (i === 1) {
-                        index = "";
-                    }
-                    if (model.get("imageUrl" + index)) {
+                _.each(modelArray, $.proxy(function(model) {
+                    if (model.get("imageUrl")) {
+                        var imagePath = model.get("imagePath") ? model.get("imagePath") + "/" : "";
                         imgArray.push({
-                            imageUrl : model.get("imageUrl" + index),
-                            imageComment : model.get("imageComment" + index),
+                            imageUrl : imagePath + model.get("imageUrl"),
+                            imageComment : model.get("imageComment"),
+                            imageIndex : index
+                        });
+                    } else {
+                        $($(this.el).find(".eventFileImage img")[index - 1]).parent().parent().hide();
+                    }
+                    index++;
+                }, this));
+            } else {
+                // その他の場合（イベント記事の場合）はモデル自身がもつ画像情報を対象とする
+                for (var i = 1; i < 4; i++) {
+                    var indexPrefix = i;
+                    if (i === 1) {
+                        indexPrefix = "";
+                    }
+                    if (this.model.get("imageUrl" + indexPrefix)) {
+                        imgArray.push({
+                            imageUrl : this.model.get("imageUrl" + indexPrefix),
+                            imageComment : this.model.get("imageComment" + indexPrefix),
                             imageIndex : i
                         });
                     } else {
                         $($(this.el).find(".eventFileImage img")[i - 1]).parent().parent().hide();
                     }
                 }
-            }, this));
+            }
             this.showPIOImages(".eventFileImage img", imgArray, true, $.proxy(this.onClickImage, this));
         },
     });
