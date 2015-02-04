86c912fe0d4a733e5c968363cc2cd10b4c216b3b
tomohiro <tomohiro.ito@jp.fujitsu.com>
2015-02-04 17:18:11 +0900

    NAM-757,758 達成情報取得、データ変換処理
    * 動画情報にタイトルを付加するような処理を追加
    * 全情報から当該ユーザのレコードを作成するモデル作成
    * utilにcsvにデータを変換する関数追加
    
    担当：伊藤

---
+++ www_dev/app/modules/model/misc/DojoAchievementCsvModel.js
@@ -0,0 +1,101 @@
+define(function(require, exports, module) {
+    "use strict";
+
+    var app = require("app");
+    var Code = require("modules/util/Code");
+    var AbstractModel = require("modules/model/AbstractModel");
+    var DojoContentModel = require("modules/model/dojo/DojoContentModel");
+
+    /**
+     * 道場達成状況CSVのモデルクラスを作成する。
+     * 
+     * @class 道場達成状況CSVのモデルクラス
+     * @exports DojoAchievementCsvModel
+     * @constructor
+     */
+    var DojoAchievementCsvModel = AbstractModel.extend({
+        /**
+         * 道場動画コレクション
+         * @memberOf DojoAchievementCsvModel#
+         */
+        dojoContentCollection : null,
+        /**
+         * 達成状況コレクション
+         * @memberOf DojoAchievementCsvModel#
+         */
+        achievementCollection : null,
+        /**
+         * 道場達成状況CSVに出力する習得ラベル
+         * @memberOf DojoAchievementCsvModel#
+         */
+        DOJO_CSV_SOLVED_LABEL : "習得済",
+        /**
+         * 道場達成状況CSVに出力する視聴ラベル
+         * @memberOf DojoAchievementCsvModel#
+         */
+        DOJO_CSV_WATCH_LABEL : "視聴済",
+        /**
+         * 道場達成状況CSVに出力する未視聴ラベル
+         * @memberOf DojoAchievementCsvModel#
+         */
+        DOJO_CSV_UNWATCH_LABEL : "未視聴",
+
+        /**
+         * パーソナル情報から、当該ユーザの道場達成状況CSVを1レコード生成
+         * @param {Model} personalModel
+         * @memberOf DojoAchievementCsvModel#
+         */
+        createDojoAchievementCsvData : function(personalModel) {
+            if (!this.dojoContentCollection || !this.achievementCollection) {
+                app.logger.debug("this method needs dojoContentCollection,achievementCollection");
+                return;
+            }
+            var csvObject = {};
+            this.personalModel = personalModel;
+            csvObject.userId = personalModel.get("__id");
+            this.dojoContentCollection.each(function(dojoModel) {
+                var csvTitleLabel = this.getCsvTitleLabel(dojoModel);
+                var csvAchievementLabel = this.getCsvAchievementLabel(dojoModel);
+                csvObject[csvTitleLabel] = csvAchievementLabel;
+            }.bind(this));
+            return csvObject;
+        },
+        /**
+         * 道場モデルからCSVタイトル用のラベルを生成
+         * @param {Model} dojoModel
+         * @memberOf DojoAchievementCsvModel#
+         */
+        getCsvTitleLabel : function(dojoModel) {
+            var levelObject = Code.DOJO_LEVELS[dojoModel.get("level")];
+            var levelLabel = "";
+            if (levelObject) {
+                levelLabel = levelObject.label + "：";
+            }
+            return levelLabel + dojoModel.get("title");
+        },
+        /**
+         * 道場モデルから当該ユーザのCSV達成状況値用のラベルを生成
+         * @param {Model} dojoModel
+         * @memberOf DojoAchievementCsvModel#
+         */
+        getCsvAchievementLabel : function(dojoModel) {
+            var myDojoModel = $.extend(true, {}, dojoModel);
+            // 当該ユーザのみの達成状況に絞る
+            myDojoModel.achievementModels = _.filter(dojoModel.achievementModels, function(model) {
+                return model.get("userId") === this.personalModel.get("__id");
+            }.bind(this));
+            var solveStatus = myDojoModel.getSolvedState();
+            if (solveStatus === Code.DOJO_STATUS_SOLVED) {
+                return this.DOJO_CSV_SOLVED_LABEL;
+            } else {
+                var watchStatus = myDojoModel.getWatchedState();
+                if (watchStatus === Code.DOJO_STATUS_WATCHED) {
+                    return this.DOJO_CSV_WATCH_LABEL;
+                }
+            }
+            return this.DOJO_CSV_UNWATCH_LABEL;
+        },
+    });
+
+    module.exports = DojoAchievementCsvModel;
+});
