c8338b60efd84bdf674cda59054558467c1852c3
Manabu Sakaguchi <sakaguchi.manab@jp.fujitsu.com>
2015-02-09 21:00:47 +0900

    NAM-1002 放射線：アプリ基盤作成
    放射線アプリのCordovaプロジェクトを登録。
    必要と思われるCordova Pluginも含めている。
    担当：阪口

---
+++ cordova/rad/plugins/jp.fukushima.namie.town.PcsAccountManagerPlugin/src/android/AuthenticatorService.java
@@ -0,0 +1,23 @@
+package jp.fukushima.namie.town.PcsAccountManagerPlugin;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AuthenticatorService extends Service {
+    private Authenticator auth;
+
+    @Override
+    public void onCreate() {
+        auth = new Authenticator(this);
+    }
+
+    @Override
+    public void onDestroy() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return auth.getIBinder();
+    }
+}
