eaaaeda14094e98cc3912aaaa72309b25045dc7e
kurosawa <fj8730jh@aa.jp.fujitsu.com>
2015-02-13 17:41:47 +0900

    NAM-1097 キッティングエラー調査
    サーバチェックスレッドのエラー処理を行った。
    
    担当：黒澤

--- cordova/news/platforms/android/src/jp/fukushima/namie/town/news/PublishStatusInitializeThread.java
+++ cordova/news/platforms/android/src/jp/fukushima/namie/town/news/PublishStatusInitializeThread.java
@@ -18,21 +18,30 @@ public class PublishStatusInitializeThread extends AbstractRequestThread {
         Log.i(TAG, "PublishStatusInitializeThread started.");
         
         PublishStatus publishStatus = PublishStatus.getInstance();
+        
+        try {
+            // 配信時刻と休刊日情報を取得し、既読済み情報などを初期化する
+            PersoniumModel personium = new PersoniumModel();
+            // アカウントが設定されていない場合、情報取得は行わない
+            if (personium.getAuthToken(_mContext) != null) {
+                personium.initPublishStatus(_mContext, publishStatus);
 
-        // 配信時刻と休刊日情報を取得し、既読済み情報などを初期化する
-        PersoniumModel personium = new PersoniumModel();
-        // アカウントが設定されていない場合、情報取得は行わない
-        if (personium.getAuthToken(_mContext) != null) {
-            personium.initPublishStatus(_mContext, publishStatus);
+                WidgetContentManager contentManager = _mWidgetProvider.getContentManager();
+                if(contentManager != null){
+                    // おすすめ記事の取得
+                    getRecommendArticles(contentManager);
 
-            // おすすめ記事の取得
-            WidgetContentManager contentManager = _mWidgetProvider.getContentManager();
-            getRecommendArticles(contentManager);
+                    // 設定情報(COLOR_LABEL)の取得
+                    String colorLabel = personium.getColorLabel(_mContext);
+                    if(colorLabel != null){
+                        contentManager.setSiteMap(colorLabel);
+                    }
+                }
+            }
+        } catch(Exception e){
+            Log.e(TAG, "PublishStatusInitializeThread error", e);
+        }
 
-            // 設定情報(COLOR_LABEL)の取得
-            String colorLabel = personium.getColorLabel(_mContext);
-            contentManager.setSiteMap(colorLabel);
-		}
         publishStatus.isRefreshing = false;
         Log.i(TAG, "PublishStatusInitializeThread completed.");
     }
