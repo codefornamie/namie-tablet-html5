1d06417e843434e31a37d6492a6f0cbef267c6dd
Manabu Sakaguchi <sakaguchi.manab@jp.fujitsu.com>
2015-02-19 18:06:19 +0900

    NAM-219 品質：単体テスト作成（タブレット）
    * NewspaperHolidayCollectionのテストを追加
    担当：阪口

--- www_dev/test/mocha/specs/collection/misc/NewspaperHolidayCollection.spec.js
+++ www_dev/test/mocha/specs/collection/misc/NewspaperHolidayCollection.spec.js
@@ -16,6 +16,7 @@ define(function(require) {
             SpecHelper.before(this, done);
         });
         var fetchedModel = null;
+        var newspaperHolidayCollection;
         it("TEST-01 NewspaperHolidayCollection#fetch, 休刊日情報が取得できることを確認する。", function(done) {
             this.timeout(20000);
             testDataId = "2015-04-29";
@@ -31,6 +32,22 @@ define(function(require) {
                     assert.equal(fetchedModel.get("__id"), testDataId, "fetched correct NewspaperHoliday model.");
                     done();
                 }
+            });
+            newspaperHolidayCollection = collection;
+        });
+        it("TEST-02 NewspaperHolidayCollection#prevPublished, 直近の発刊日を取得できることを確認する。", function(done) {
+            newspaperHolidayCollection.prevPublished(new Date("2015-05-07"), function(prevDate, err) {
+                app.logger.debug("prevDate:" + prevDate.toISOString());
+                assert.equal(prevDate.toISOString(), "2015-05-01T00:00:00.000Z", "prevPublished is correct.");
+                done();
+            });
+        });
+        it("TEST-03 NewspaperHolidayCollection#nextPublish, 直近の次号発刊日を取得できることを確認する。", function(done) {
+            newspaperHolidayCollection.nextPublish(new Date("2015-05-01"), function(prevDate, err) {
+                app.logger.debug("prevDate:" + prevDate.toISOString());
+                assert.equal(prevDate.toISOString(), "2015-05-01T00:00:00.000Z", "prevPublished is correct.");
+                assert.equal(err, undefined, "nextPublish is correct.");
+                done();
             });
         });
     });
