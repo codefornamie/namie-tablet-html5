025026cbea566c4518232e0feb72b2ab05e7d9e5
Manabu Sakaguchi <sakaguchi.manab@jp.fujitsu.com>
2015-02-19 14:31:54 +0900

    NAM-219 品質：単体テスト作成（タブレット）
    * 削除後の存在確認を行うテストを追加
    担当：阪口

--- www_dev/test/mocha/specs/model/article/ArticleModel.spec.js
+++ www_dev/test/mocha/specs/model/article/ArticleModel.spec.js
@@ -14,11 +14,12 @@ define(function(require) {
         });
 
         var testDataId;
-        it("TEST-01 ArticleModel#save", function(done) {
+        var type = "6";
+        var category;
+        it("TEST-01 ArticleModel#save, 記事が保存ができることを確認する。", function(done) {
 
             var model = new ArticleModel();
-            var type = "6";
-            var category = _.find(Code.ARTICLE_CATEGORY_LIST, function(category) {
+            category = _.find(Code.ARTICLE_CATEGORY_LIST, function(category) {
                 return category.key === type;
             });
             var publishedAt = moment(new Date()).format("YYYY-MM-DD");
@@ -41,7 +42,7 @@ define(function(require) {
             });
         });
         var fetcheModel;
-        it("TEST-02 ArticleModel#fetch", function(done) {
+        it("TEST-02 ArticleModel#fetch, 作成した記事の取得ができることを確認する。", function(done) {
             fetcheModel = new ArticleModel();
             fetcheModel.set("__id", testDataId);
 
@@ -49,11 +50,14 @@ define(function(require) {
                 success : function(model, response, options) {
                     assert.notEqual(model, undefined, 'fetched model is not undefined.');
                     assert.equal(fetcheModel.get("__id"), testDataId, "fetched model's id is correct.");
+                    assert.equal(fetcheModel.get("type"), type, "fetched model's type is correct.");
+                    assert.equal(fetcheModel.get("site"), category.value, "fetched model's site is correct.");
+                    assert.equal(fetcheModel.get("title"), "UnitTestData", "fetched model's type is correct.");
                     done();
                 }
             });
         });
-        it("TEST-03 ArticleModel#destory", function(done) {
+        it("TEST-03 ArticleModel#destory, 記事が削除ができることを確認する。", function(done) {
             fetcheModel.destroy({
                 success : function(model, response, options) {
                     assert.ok(true, "Success delete test data.");
@@ -61,6 +65,19 @@ define(function(require) {
                 },
                 error : function() {
                     assert.ok(false, "Failed delete test data.");
+                    done();
+                }
+            });
+        });
+        it("TEST-04 ArticleModel#fetch, 削除した記事の取得ができないことを確認する。", function(done) {
+            fetcheModel = new ArticleModel();
+            fetcheModel.set("__id", testDataId);
+
+            fetcheModel.fetch({
+                success : function(model, response, options) {
+                    app.logger.debug("response:" + JSON.stringify(response));
+                    assert.equal(response.code, "PR404-OD-0002", "fetched model's type is correct.");
+                    assert.ok(true, "not found delete test data.");
                     done();
                 }
             });
