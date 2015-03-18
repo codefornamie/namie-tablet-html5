524e717aa9b6c72adf786bfc60fe3596db6c3942
Manabu Sakaguchi <sakaguchi.manab@jp.fujitsu.com>
2015-03-18 16:42:54 +0900

    NAM-1281 納品対応：設計書作成：なみえ新聞
    * appConfig.jsをコミットしていたため差し戻し
    担当：阪口

--- www_dev/app/resources/appConfig.js
+++ www_dev/app/resources/appConfig.js
@@ -7,7 +7,7 @@ define(function(require, exports, module) {
             "odataName" : "odata",
             "retryCount" : "3",
             // view category (ex. news, dojo, letter, rad, posting, ope ..).
-            "mode" : "letter"
+            "mode" : "news"
         },
         "logger" : {
             "enable" : true,
